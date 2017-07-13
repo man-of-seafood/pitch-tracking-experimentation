87b295338efd95bcfc3e1c2b4fbd55bcbad27340
Mason Raasch <raasch.mason@gmail.com>
2017-07-13 11:47:39 -0700

    add shorter sound file so that silences have minimal impact on the null/non-null ratio. find more or less configuration options for the pitch detector

--- test-pitch-analyzer.js
+++ test-pitch-analyzer.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const Promise = require('bluebird');
+const fs = require('fs');
 Promise.promisifyAll(fs);
 const WavDecoder = require('wav-decoder');
 const Pitchfinder = require('pitchfinder');
@@ -10,11 +10,11 @@ const amdfDetector = new Pitchfinder.AMDF();
 const detectors = [yinDetector, amdfDetector];
 
 const detectionConfig = {
-  tempo: 120,
+  tempo: 1200,
   quantization: 8
 };
 
-fs.readFileAsync('./amy.wav')
+fs.readFileAsync('./short-sound-im-amy.wav')
   .then(buffer => {
     return WavDecoder.decode(buffer)
   })
@@ -25,7 +25,9 @@ fs.readFileAsync('./amy.wav')
       console.log(curr);
       return acc + (curr === null ? 0 : 1);
     }, 0);
-    console.log(nonNull)
+    console.log('total length:', frequencies.length);
+    console.log('non-null', nonNull);
+    console.log('ratio:', nonNull/frequencies.length);
     //now write the function that maps each of pitch readings to an object with x and y vals (x = time, y = frequency)
     //we also need to know the time between pitch readings -- bpm/60 * quantization
     const deltaT = 1/((detectionConfig.tempo/60) * detectionConfig.quantization);
@@ -38,4 +40,17 @@ fs.readFileAsync('./amy.wav')
     console.log(coords);
   })
   .catch(err => console.error('err', err));
+/*
+General idea for smoothing: Make a note of the last known coordinate, then run until there's another valid coordinate, calcuate the pitch change/time change,
+yielding the number of hz that each node in between should be incremented by.
 
+make a note of the start and end points of null runs 
+then iterate over those portions specifically
+however -- if the start is a null run or the end is a null run, then you're going to want to make all of those coords have the same value since nothing can be inferred
+unless you look at the slope of a series of the next 3/4 coords, but then you're basically starting to make your own smoothing algo
+
+another consideration: nulls could just be from silence -- see if the ranges map up
+
+or 
+remove all the coordinates up until the point we have a minimum of X consecutive non-null entries (like we know we're on the right track)
+*/
